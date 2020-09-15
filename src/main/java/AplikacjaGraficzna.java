2a6d8bdcf3cd4fe860c21d70b469a2e9f87d959e
JustynaKuznikk <justyna.kuznikk@gmail.com>
2020-09-15 17:21:01 +0200

    opcje ustawiania stage

--- src/main/java/AplikacjaGraficzna.java
+++ src/main/java/AplikacjaGraficzna.java
@@ -3,6 +3,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class AplikacjaGraficzna extends Application {
 
@@ -15,7 +16,7 @@ public class AplikacjaGraficzna extends Application {
     public void start(Stage primaryStage) {
         //Stage ->    Scene ->    StackPane ->    Text
 
-        Text text = new Text("Tekst z pierwszej aplikacji graficznej");
+        Text text = new Text("Moja aplikacja graficzna");
 
         //StackPane == root(korzeń/początek)
         StackPane stackPane = new StackPane();
@@ -25,6 +26,22 @@ public class AplikacjaGraficzna extends Application {
         Scene scene = new Scene(stackPane);
 
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Moja aplikacja");
+        //ustawianie współrzędnych ustawiania okna
+        primaryStage.setX(500);
+        primaryStage.setY(300);
+        //ustawienie wysokości i szerokości okna
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(300);
+        //ustawianie stylu
+        primaryStage.initStyle(StageStyle.DECORATED);
+        //primaryStage.initStyle(StageStyle.UNDECORATED);
+        //primaryStage.initStyle(StageStyle.UNIFIED);
+        //primaryStage.initStyle(StageStyle.UTILITY);
+        //primaryStage.initStyle(StageStyle.TRANSPARENT);
+        //okienko na cała szerokość ekranu
+        //primaryStage.setFullScreen(true);
+
         primaryStage.show();
 
     }
