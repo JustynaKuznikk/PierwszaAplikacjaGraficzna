fdda45a7c81b6cf0ec08b0ad29cd5b63841f36b5
JustynaKuznikk <justyna.kuznikk@gmail.com>
2020-09-15 16:59:10 +0200

    wyświetlanie pierwszego tekstu

--- src/main/java/AplikacjaGraficzna.java
+++ src/main/java/AplikacjaGraficzna.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class AplikacjaGraficzna extends Application {
@@ -7,8 +10,21 @@ public class AplikacjaGraficzna extends Application {
         launch(args);
     }
 
+    //primaryStage to nasze główne okno
     @Override
     public void start(Stage primaryStage) {
+        //Stage ->    Scene ->    StackPane ->    Text
+
+        Text text = new Text("Tekst z pierwszej aplikacji graficznej");
+
+        //StackPane == root(korzeń/początek)
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(text);
+
+        //płótno do rysowania obiektów
+        Scene scene = new Scene(stackPane);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
 
     }
