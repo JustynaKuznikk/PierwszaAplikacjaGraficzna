da11ed3f9122fedaab7784c42c067a1de0fa6818
unknown <justyna.kuznikk@gmail.com>
2020-10-05 12:45:50 +0200

    BorderPane

--- src/main/java/UkladWidoku.java
+++ src/main/java/UkladWidoku.java
@@ -1,6 +1,8 @@
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class UkladWidoku extends Application {
@@ -11,9 +13,21 @@ public class UkladWidoku extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        //BorderPane
+        BorderPane borderPane = new BorderPane();
+        TextField textFieldTop = new TextField("Top");
+        borderPane.setTop(textFieldTop);
+        TextField textFieldBottom = new TextField("Bottom");
+        borderPane.setBottom(textFieldBottom);
+        TextField textFieldCenter = new TextField("Center");
+        borderPane.setCenter(textFieldCenter);
+        TextField textFieldLeft = new TextField("Left");
+        borderPane.setLeft(textFieldLeft);
+        TextField textFieldRight = new TextField("Right");
+        borderPane.setRight(textFieldRight);
 
-        Group group = new Group();
-        Scene scene = new Scene(group,300,300);
+
+        Scene scene = new Scene(borderPane,500,500);
         primaryStage.setTitle("Layout");
         primaryStage.setScene(scene);
         primaryStage.show();
