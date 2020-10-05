09c3182d38fa3aed7e3ae7872de32672eba4b088
unknown <justyna.kuznikk@gmail.com>
2020-10-05 13:22:04 +0200

    VBox

--- src/main/java/UkladWidoku.java
+++ src/main/java/UkladWidoku.java
@@ -1,10 +1,14 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class UkladWidoku extends Application {
@@ -16,20 +20,39 @@ public class UkladWidoku extends Application {
     @Override
     public void start(Stage primaryStage) {
 
+
         //HBox
         Button menuButton = new Button("Menu");
         Button contactButton = new Button("Contact");
         Button orderButton = new Button("Order");
         HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        hBox.setPadding(new Insets(10,5,10,5));
+        hBox.setBackground(new Background(new BackgroundFill(Color.LIGHTSEAGREEN, CornerRadii.EMPTY,Insets.EMPTY)));
         hBox.getChildren().addAll(menuButton,contactButton,orderButton);
+
+        //VBox
+        VBox vBox = new VBox();
+        ImageView pizzaImageView = new ImageView(new Image("obrazki/ikony/pizza.png"));
+        Label pizzaLabel = new Label("Pizza",pizzaImageView);
+        ImageView hamburgerImageView = new ImageView(new Image("obrazki/ikony/hamburger.png"));
+        Label hamburgerLabel = new Label("Hamburger",hamburgerImageView);
+        ImageView drinksImageView = new ImageView(new Image("obrazki/ikony/napoje.png"));
+        Label drinksLabel = new Label("Drinks",drinksImageView);
+        vBox.getChildren().addAll(pizzaLabel,hamburgerLabel,drinksLabel);
+        vBox.setSpacing(10);
+        vBox.setPadding(new Insets(10,10,10,10));
+        vBox.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY,Insets.EMPTY)));
+
 
 
         //BorderPane
         BorderPane borderPane = new BorderPane();
         borderPane.setTop(hBox);
+        borderPane.setLeft(vBox);
 
 
-        Scene scene = new Scene(borderPane,500,500);
+        Scene scene = new Scene(borderPane,400,400);
         primaryStage.setTitle("Layout");
         primaryStage.setScene(scene);
         primaryStage.show();
